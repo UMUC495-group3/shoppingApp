94a05c9e6e92dd16eb1051559fede2ea23573751
Tyler Roland <tyler.roland92@yahoo.com>
2017-06-02 17:34:57 -0400

    Modified database structure for single user
    Removed Users table, added "Recurring" column to the Products table, removed "UserID", "ListItems", and "ListPrice" columns from the Lists table, and added "ProductID" column to the Lists table.

--- New Folder/umuccmsc_groupdb.sql
+++ New Folder/umuccmsc_groupdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jun 01, 2017 at 05:53 PM
+-- Generation Time: Jun 02, 2017 at 05:28 PM
 -- Server version: 10.0.27-MariaDB-cll-lve
 -- PHP Version: 5.4.31
 
@@ -28,10 +28,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `cmsc_lists` (
   `ListID` int(11) NOT NULL AUTO_INCREMENT,
-  `UserID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
   `ListDate` date NOT NULL,
-  `ListItems` varchar(500) NOT NULL,
-  `ListPrice` decimal(10,0) NOT NULL,
   PRIMARY KEY (`ListID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -43,25 +41,10 @@ CREATE TABLE IF NOT EXISTS `cmsc_lists` (
 
 CREATE TABLE IF NOT EXISTS `cmsc_products` (
   `ProductID` int(11) NOT NULL AUTO_INCREMENT,
-  `ProductDescription` varchar(60) NOT NULL,
+  `ProductName` varchar(60) NOT NULL,
   `ProductPrice` decimal(10,0) NOT NULL,
+  `Recurring` tinyint(1) NOT NULL,
   PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cmsc_users`
---
-
-CREATE TABLE IF NOT EXISTS `cmsc_users` (
-  `UserID` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(100) NOT NULL,
-  `Password` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Recurrences` varchar(500) NOT NULL,
-  PRIMARY KEY (`UserID`),
-  UNIQUE KEY `Username` (`Username`,`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
