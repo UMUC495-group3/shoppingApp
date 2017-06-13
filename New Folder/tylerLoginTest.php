469f80463d2eae9a33d4db409c45fb262419b039
Tyler Roland <tyler.roland92@yahoo.com>
2017-06-13 15:33:18 -0400

    Edited Login Test script
    Added the redirect so that when the time comes to add the script to each page, the commented code can be easily be switched and implemented.

--- New Folder/tylerLoginTest.php
+++ New Folder/tylerLoginTest.php
@@ -1,8 +1,10 @@
 <?php
 	session_start();
 
-	if (!isset($_SESSION['login']) || !isset($_SESSION['username']) || $_SESSION['login'] != 1 || $_SESSION['username'] == '')
+	if (!isset($_SESSION['login']) || !isset($_SESSION['username']) || $_SESSION['login'] != 1 || $_SESSION['username'] == '') {
 		echo "Not logged in.";
+		//header("Location: login.php");
+	}
 	else 
 		echo "Logged in as: " . $_SESSION['username'];
 
